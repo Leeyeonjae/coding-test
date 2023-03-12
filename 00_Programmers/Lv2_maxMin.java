0671613bbc3f71223c0f844acc0c7178c7eb9b9f
Yeonjae Lee <thisyj98@gmail.com>
2023-03-12 19:09:53 +0900

    Modify Problem [Maximum Minimum]

--- 00_Programmers/Lv2_maxMin.java
+++ 00_Programmers/Lv2_maxMin.java
@@ -4,9 +4,8 @@ class Solution {
     public String solution(String s) {
         int[] numArr = Arrays.stream(s.split(" "))
                         .mapToInt(Integer::parseInt)
+                        .sorted()
                         .toArray();
-        
-        Arrays.sort(numArr);
         
         return (numArr[0] + " " + numArr[numArr.length - 1]);
     }
